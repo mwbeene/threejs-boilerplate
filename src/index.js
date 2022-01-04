5bfc9210959f2f3f3b7f89f57249a50b41a67546
Mike Beene <mwbeene@gmail.com>
2022-01-04 16:40:51 -0500

    Added boilerplate scene

--- src/index.js
+++ src/index.js
@@ -1,18 +1,51 @@
-import _ from 'lodash';
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
-const scene = new THREE.Scene();
+let camera, scene, renderer;
 
-console.log( scene );
-console.log( 'test' );
+let controls;
 
-function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+let mesh;
+
+init();
+animate();
+
+function init(){
+  camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, .01, 10 );
+  camera.position.z = 4;
+
+  scene = new THREE.Scene();
+
+  const geometry = new THREE.BoxGeometry( 2, 2, 2 );
+  const material = new THREE.MeshNormalMaterial();
+
+  mesh = new THREE.Mesh( geometry, material );
+  scene.add( mesh );
+
+  renderer = new THREE.WebGLRenderer( { antialias: true } );
+  renderer.setPixelRatio( window.devicePixelRatio );
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  document.body.appendChild( renderer.domElement );
+
+  controls = new OrbitControls( camera, renderer.domElement );
+  controls.enableDamping = true;
+  controls.dampingFactor = 0.05;
+  controls.screenSpacePanning = false;
+  controls.minDistance = 1;
+  controls.maxDistance = 5;
+
+  window.addEventListener( 'resize', onWindowResize );
+}
+
+function onWindowResize(){
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize( window.innerWidth, window.innerHeight );
+}
+
+function animate(){
+  requestAnimationFrame( animate );
+  controls.update();
+  renderer.render( scene, camera );
+}
